572e435de7b0a34b826b919320d45c53a9a286f6
RvDmitry <rvdmitry@gmail.com>
2020-04-06 21:19:13 +0600

    2. Тесты для идеальный вес[#266407] исправил тип параметра in.

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -7,7 +7,7 @@ public class FitTest {
 
     @Test
     public void whenManHeight180Then92Weight() {
-        int in = 180;
+        double in = 180;
         double expected = 92;
         double out = Fit.manWeight(in);
         Assert.assertEquals(expected, out, 0.01);
@@ -15,7 +15,7 @@ public class FitTest {
 
     @Test
     public void whenWomanHeight170Then69Weight() {
-        int in = 170;
+        double in = 170;
         double expected = 69;
         double out = Fit.womanWeight(in);
         Assert.assertEquals(expected, out, 0.01);
---
+++ test/ru/job4j/condition/PointTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class PointTest {
+
+    @Test
+    public void distance() {
+    }
+}
