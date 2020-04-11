93d05e420adc03464afe7c1d9ba8cc9eccc028b5
RvDmitry <rvdmitry@gmail.com>
2020-04-11 13:21:50 +0600

    6.1. Классический поиск перебором.[#266325]

---
+++ test/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas10Then1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 10;
+        int result = find.indexOf(input, value);
+        int expect = 1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas15Then4() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3, 4, 15, 12};
+        int value = 15;
+        int result = find.indexOf(input, value);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasNot9ThenMinus1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3, 4, 15, 12};
+        int value = 9;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
