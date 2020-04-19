710d75af8afb90ea8a388cf6b7c023ad3797a158
RvDmitry <rvdmitry@gmail.com>
2020-04-19 12:06:32 +0600

    1.1. Создание объекта[#266481]

---
+++ src/ru/job4j/oop/Cat.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+/**
+ * Class Cat
+ * В классе практикуется создание объектов класса.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class Cat {
+    /**
+     * Главный метод программы. В методе создаются экземпляры класса Cat
+     * @param args Параметры командной строки
+     */
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ src/ru/job4j/oop/Dog.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+/**
+ * Class Dog
+ * В классе практикуется создание объектов класса.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class Dog {
+    /**
+     * Главный метод программы. Практикуется создание экземпляров класса Dog.
+     * @param args Параметры командной строки
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
