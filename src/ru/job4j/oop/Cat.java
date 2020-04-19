37294e217bedf141a266698f01181437f11fc125
RvDmitry <rvdmitry@gmail.com>
2020-04-19 15:46:01 +0600

    1.5. Поля объекта[#266482]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -2,11 +2,44 @@ package ru.job4j.oop;
 
 /**
  * Class Cat
- * В классе практикуется создание объектов класса и вызов его метода.
+ * В классе практикуется создание объектов класса и вызов его методов.
  * @author Dmitry Razumov
- * @version 1
+ * @version 2
  */
 public class Cat {
+    /**
+     * Поле задает еду которую котик съел.
+     */
+    private String food;
+
+    /**
+     * Поле задает кличку котика.
+     */
+    private String name;
+
+    /**
+     * Метод выводит имя котика и что он съел.
+     */
+    public void show() {
+        System.out.printf("Котик %s съел %s \n", this.name, this.food);
+    }
+
+    /**
+     * Метод задает еду, которую съел котик.
+     * @param meat Еда
+     */
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    /**
+     * Метод задает кличку котика.
+     * @param nick Кличка
+     */
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     /**
      * Метод возвращает стороку.
      * @return Строка
@@ -17,13 +50,20 @@ public class Cat {
     }
 
     /**
-     * Главный метод программы. В методе создаются экземпляры класса Cat и вызывается его метод.
+     * Главный метод программы. В методе создаются экземпляры класса Cat и вызывается его методы.
+     * Результаты работы методов выводятся на консоль.
      * @param args Параметры командной строки
      */
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Феликс");
+        black.eat("fish");
+        black.show();
     }
 }
