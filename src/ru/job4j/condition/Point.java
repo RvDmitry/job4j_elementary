401b43ee226550e0b22d0f15261a316e5cb6c99b
RvDmitry <rvdmitry@gmail.com>
2020-04-19 22:55:44 +0600

    2.1. Рефакторинг - Расстояние между точками.[#266477]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,22 +2,51 @@ package ru.job4j.condition;
 
 /**
  * Class Point
- * Класс расчитывает расстояние между двумя точками.
+ * Класс рассчитывает расстояние между двумя точками.
  * @author Dmitry Razumov
- * @version 1
+ * @version 2
  */
 public class Point {
+    /**
+     * Координата X.
+     */
+    private int x;
+
+    /**
+     * Координата Y.
+     */
+    private int y;
+
+    /**
+     * Конструктор - инициализирует начальные координаты точки.
+     * @param first Координата X
+     * @param second Координата Y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    /**
+     * Метод вычисляет расстояние между двумя точками.
+     * На вход метода подается точка относительно которой нужно рассчитать расстояние от текущей.
+     * @param that Объект - точка
+     * @return Расстояние между точками
+     */
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+
     /**
      * Метод вычисляет расстояние между двумя точками.
      * @param x1 Координа х первой точки
      * @param y1 Координата у первой точки
      * @param x2 Координата х второй точки
      * @param y2 Координата у второй точки
-     * @return Расстояние
+     * @return Расстояние между точками
      */
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     /**
@@ -25,11 +54,9 @@ public class Point {
      * @param args Параметры командной строки
      */
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(0, 1, 5, 5);
-        System.out.println("result (0, 1) to (5, 5) " + result);
-        result = Point.distance(2, 2, 4, 10);
-        System.out.println("result (2, 2) to (4, 10) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -7,13 +7,22 @@ import ru.job4j.calculator.Fit;
 public class PointTest {
 
     @Test
-    public void distance() {
+    public void whenDistanceCoordinate() {
         int x1 = 2;
         int y1 = 2;
         int x2 = 6;
         int y2 = 5;
         double expected = 5;
         double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenDistancePoint() {
+        Point a = new Point(2, 2);
+        Point b = new Point(6, 5);
+        double expected = 5;
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
