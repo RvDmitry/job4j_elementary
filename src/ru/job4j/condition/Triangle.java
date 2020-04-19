5362af8d4eb16aa95f990b5697b386b05b93bdce
RvDmitry <rvdmitry@gmail.com>
2020-04-20 00:33:55 +0600

    2.2. Рефакторинг - Площадь треугольника.[#266478]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -2,20 +2,72 @@ package ru.job4j.condition;
 
 /**
  * Class Triangle
- * Класс проверяет существует ли треугольник.
+ * Класс проверяет существует ли треугольник и если существует, то вычисляет его площадь.
  * @author Dmitry Razumov
- * @version 1
+ * @version 2
  */
 public class Triangle {
     /**
+     * Первая точка треугольника.
+     */
+    private Point first;
+    /**
+     * Вторая точка треугольника.
+     */
+    private Point second;
+    /**
+     * Третья точка треугольника.
+     */
+    private Point third;
+
+    /**
+     * Конструктор создает треугольник по трем точкам.
+     * @param ap Первая точка
+     * @param bp Вторая точка
+     * @param cp Третья точка
+     */
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    /**
+     * Метод вычисляет периметр по длинам сторон.
+     * @param a Расстояние между точками a b
+     * @param c Расстояние между точками a c
+     * @param b Расстояние между точками b c
+     * @return Периметр.
+     */
+    public double period(double a, double c, double b) {
+        return (a + c + b) / 2;
+    }
+
+    /**
+     * Метод вычисляет площадь треугольника.
+     * @return Площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double c = first.distance(third);
+        double b = second.distance(third);
+        double p = period(a, c, b);
+        if (this.exist(a, c, b)) {
+            rsl = Math.sqrt(p * (p - a) * (p - c) * (p - b));
+        }
+        return rsl;
+    }
+
+    /**
      * Метод проверяет существует ли треугольник. Проверка заключается в оценке всех сторон треугольника.
      * Правило оценки: сумма любой из двух сторон всегда больше оставшейся стороны.
-     * @param ab Сторона AB
-     * @param ac Сторона AC
-     * @param bc Сторона BC
+     * @param a Длина от точки a b
+     * @param c Длина от точки a c
+     * @param b Длина от точки b c
      * @return true, если треугольник существует, false, если не существует
      */
-    public static boolean exist(double ab, double ac, double bc) {
-        return (ab + ac) > bc && (ac + bc) > ab && (ab + bc) > ac;
+    private boolean exist(double a, double c, double b) {
+        return (a + c) > b && (c + b) > a && (a + b) > c;
     }
 }
--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,19 +1,29 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
+
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void whenTriangleExist() {
+        Point a = new Point(0, 2);
+        Point b = new Point(2, 2);
+        Point c = new Point(2, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double out = triangle.area();
+        double expected = 2;
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void whenNoExist() {
-        boolean result = Triangle.exist(0, 2.0, 2.0);
-        assertThat(result, is(false));
+    public void whenTriangleNoExist() {
+        Point a = new Point(0, 2);
+        Point b = new Point(2, 2);
+        Point c = new Point(2, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        double out = triangle.area();
+        double expected = -1;
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
