fcc382f9213144afde2519a8310afcdcdd778950
RvDmitry <rvdmitry@gmail.com>
2020-05-05 12:00:36 +0600

    9.1 Рефакторинг теста @Before @After[#266445]

--- test/ru/job4j/stragery/PaintTest.java
+++ test/ru/job4j/stragery/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.stragery;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -13,14 +15,39 @@ import java.util.StringJoiner;
  * Далее мы получаем массив байт из памяти, преобразуя его в стороку и сравниваем с требуемой нам строкой.
  * Затем восстанавливаем системный вывод снова в консоль.
  * @author Dmitry Razumov
- * @version 1
+ * @version 2
  */
 public class PaintTest {
+    /**
+     *  Поле содержит дефолтный вывод в консоль.
+     */
+    private final PrintStream stdout = System.out;
+
+    /**
+     * Буфер для вывода результата.
+      */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    /**
+     * Метод устанавливает вывод в буфер.
+     */
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    /**
+     * Метод устанавливает вывод в консоль.
+     */
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -34,6 +61,22 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringJoiner(System.lineSeparator())
+                                .add("   +")
+                                .add("  + +")
+                                .add(" +   +")
+                                .add("+++++++")
+                                .add("")
+                                .toString()
+                )
+        );
     }
 }
