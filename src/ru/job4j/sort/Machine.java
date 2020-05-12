1de70821ba60bcfb88ed2b968a5da87b06d35655
RvDmitry <rvdmitry@gmail.com>
2020-05-12 19:35:03 +0600

    Сдача в кофе машине. [#34741]

---
+++ src/ru/job4j/sort/Machine.java
@@ -0,0 +1,39 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+/**
+ * Class Machine
+ * Класс обрабытывает выдачу сдачи в кофе-машине.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class Machine {
+    /**
+     * Поле задает номиналы момент, сдачу которыми выплачивает машина.
+     */
+    private final int[] coins = {10, 5, 2, 1};
+
+    /**
+     * Метод осуществляет расчет и выдачу сдачи с использованием жадного алгоритма.
+     * Т.е. всегда выдаются моменты для сдачи с максимально возможным номиналом.
+     * @param money Номинал купюры, которой рассчитывается пользователь
+     * @param price Цена за кофе
+     * @return Сдача монетами
+     */
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int cashback = money - price;
+        while (cashback > 0) {
+            for (int coin : coins) {
+                if (cashback - coin >= 0) {
+                    rsl[size++] = coin;
+                    cashback -= coin;
+                    break;
+                }
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
